9d989de9834f9153f1490d9ae1ed05027813292e
dezvoltareprinoameni <dezvoltareprinoameni@gmail.com>
2024-11-12 21:54:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Redirect to "Acasa" page if the user is on the root path
+if (window.location.pathname === "/") {
+    window.location.href = "/acasa.html";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const currentPage = window.location.pathname.split("/").pop();
 
